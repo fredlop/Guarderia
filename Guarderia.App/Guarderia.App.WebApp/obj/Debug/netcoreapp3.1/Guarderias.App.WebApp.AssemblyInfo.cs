cbc787507ab28f2d633cf7aebe398227b27d98ec
Freddy Lopez <fredlop79@gmail.com>
2022-09-29 21:35:12 -0500

    Terminacion

--- Guarderia.App/Guarderia.App.WebApp/obj/Debug/netcoreapp3.1/Guarderias.App.WebApp.AssemblyInfo.cs
+++ Guarderia.App/Guarderia.App.WebApp/obj/Debug/netcoreapp3.1/Guarderias.App.WebApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Matriculas.App.WebApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Guarderias.App.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Matriculas.App.WebApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Matriculas.App.WebApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Guarderias.App.WebApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Guarderias.App.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- Guarderia.App/Guarderia.App.WebApp/obj/Debug/netcoreapp3.1/Guarderias.App.WebApp.RazorAssemblyInfo.cs
+++ Guarderia.App/Guarderia.App.WebApp/obj/Debug/netcoreapp3.1/Guarderias.App.WebApp.RazorAssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Matriculas.App.WebApp.Views")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Guarderias.App.WebApp.Views")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorLanguageVersionAttribute("3.0")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorConfigurationNameAttribute("MVC-3.0")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyNameAttribute("MVC-3.0", "Microsoft.AspNetCore.Mvc.Razor.Extensions")]
